3ccd8919e3056db0d39c0d68ca2773cff316895b
Raymond Li <contact@raymondli.tk>
2019-03-26 13:52:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $config = include('./config.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BayviewJudge Under Construction</title>
+    <title>BayviewJudge</title>
     <link rel="stylesheet" href="./css/bulma.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
@@ -33,7 +33,7 @@ $config = include('./config.php');
                     <div class="hero-body">
                         <div class="container">
                             <h1 class="title">
-                                Bayview Computer Club
+                                BayviewJudge
                             </h1>
                             <h2 class="subtitle">
                                 Libre Online Judging Platform
