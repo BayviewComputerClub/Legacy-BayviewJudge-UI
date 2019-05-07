040bbb25ab195fcdd4c5ebc12eaef9bd7382034b
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-05-07 12:20:08 -0400

    Update Logout.php

--- Auth/Logout.php
+++ Auth/Logout.php
@@ -2,6 +2,8 @@
 $config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . "/Config/config.ini");
 session_start();
 session_destroy();
-header("Location: ".$config['page_root']);
+header("Location: ".$config['page_root
+
+echo "Have a great day";
 die();
 ?>
