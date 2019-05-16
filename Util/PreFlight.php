4e23cea831e8c980092aa2828af7a10dbfff3178
Seshan Ravikumar <seshan10@me.com>
2019-05-16 11:03:03 -0400

    Stuff

--- Util/PreFlight.php
+++ Util/PreFlight.php
@@ -2,5 +2,7 @@
 // The preflight checks function should run before each page load (through .htaccess on Apache)
 // It does sanity checks for example, and can check if a user has been banned.
 
+// Silly little thing, count how many requests the
+file_put_contents("count.txt",@file_get_contents("/var/www/html/Util/count.txt")+1);
 ?>
 <!-- Preflight checks have run -->
